a515be771a46b5ab1a1bc73877c4d1e36601b34e
Jefferson Silva <jefferson9334@gmail.com>
2021-11-30 21:47:13 -0300

    Adds navigationControllerProtocol to track screen changes

--- Coordinator-Estudo/Controller/ControllerTwo/ControllerTwoCoordinator.swift
+++ Coordinator-Estudo/Controller/ControllerTwo/ControllerTwoCoordinator.swift
@@ -5,4 +5,23 @@
 //  Created by Jefferson Silva on 30/11/21.
 //
 
-import Foundation
+import UIKit
+
+class ControllerTwoCoordinator: CoordinatorProtocol {
+    
+    weak var parent: CoordinatorProtocol?
+    
+    var childCoordinators: [CoordinatorProtocol]?
+    
+    var navigationController: UINavigationController
+    
+    func start() {
+        let vc = ViewControllerNumberTwo()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+}
--- Coordinator-Estudo/Coordinator/MainCoordinator.swift
+++ Coordinator-Estudo/Coordinator/MainCoordinator.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class MainCoordinator: CoordinatorProtocol {
+class MainCoordinator: NSObject, CoordinatorProtocol {
     
     weak var parent: CoordinatorProtocol?
     
@@ -23,11 +23,12 @@ class MainCoordinator: CoordinatorProtocol {
     func start() {
         let vc = ViewController.instantiate()
         vc.coordinator = self
+        navigationController.delegate = self
         navigationController.pushViewController(vc, animated: true)
     }
     
     // MARK: - Controller Presentation
-    // Por essa função ser idêntica à anterior, acho que poderíamos usar Swift Generics aqui.
+    // Por essa função ser idêntica à próxima, acho que poderíamos usar Swift Generics aqui.
     func createControllerNumberOne() {
         let controllerOneCoordinator = ControllerOneCoordinator(navigationController: navigationController)
         childCoordinators?.append(controllerOneCoordinator)
@@ -36,8 +37,30 @@ class MainCoordinator: CoordinatorProtocol {
     }
     
     func createControllerNumberTwo() {
-        let vc = ViewControllerNumberTwo()
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
+        let controllerTwoCoordinator = ControllerTwoCoordinator(navigationController: navigationController)
+        childCoordinators?.append(controllerTwoCoordinator)
+        controllerTwoCoordinator.parent = self
+        controllerTwoCoordinator.start()
     }
+}
+
+extension MainCoordinator: UINavigationControllerDelegate {
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from),
+              !navigationController.viewControllers.contains(fromViewController) else { return }
+        
+        
+        // Não respeita o open-closed principle.
+        if let viewControllerNumberOne = fromViewController as? ViewControllerNumberOne,
+           let coordinator = viewControllerNumberOne.coordinator {
+            childDidFinish(coordinator)
+        }
+        
+        if let viewControllerNumberTwo = fromViewController as? ViewControllerNumberTwo,
+           let coordinator = viewControllerNumberTwo.coordinator {
+            childDidFinish(coordinator)
+        }
+    }
+    
 }
